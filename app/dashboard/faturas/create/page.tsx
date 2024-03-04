eb0e0233e0252333be9966e39fc9c1a762689d88
otavio xavier <otavioxaviermarinho@gmail.com>
2024-03-04 19:33:52 -0300

    modulo_14

--- app/dashboard/faturas/create/page.tsx
+++ app/dashboard/faturas/create/page.tsx
@@ -9,10 +9,10 @@ export default async function Page() {
     <main>
       <Breadcrumbs
         breadcrumbs={[
-          { label: 'Invoices', href: '/dashboard/invoices' },
+          { label: 'Invoices', href: '/dashboard/faturas' },
           {
             label: 'Create Invoice',
-            href: '/dashboard/invoices/create',
+            href: '/dashboard/faturas/create',
             active: true,
           },
         ]}
